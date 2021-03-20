2b457873e0ef249f756b3c060d2a2d4c3206ccab
Laurens <laurverh@gmail.com>
2021-03-20 11:16:21 +0100

    Added conversion to pem

--- export-ca-certs.ps1
+++ export-ca-certs.ps1
@@ -1,16 +1,25 @@
 # https://gist.github.com/zakird/a8582ced2f50cfe1c702
 
+write-host "Running export-ca-certs.ps1."
+
 $type = [System.Security.Cryptography.X509Certificates.X509ContentType]::Cert
 
 $output_dir = "$(Get-Location)\certificates"
 
-New-Item -Force -ItemType "directory" -Path "$output_dir"
+New-Item -Force -ItemType "directory" -Path "$output_dir\der"
+New-Item -Force -ItemType "directory" -Path "$output_dir\pem"
+New-Item -Force -ItemType "directory" -Path "$output_dir\crt"
 
 get-childitem -path cert:\LocalMachine\AuthRoot | ForEach-Object {
 	$hash = $_.GetCertHashString()
 	$friendly_name = $_.FriendlyName
-	$issuer_name = $_.IssuerName.Name
-	$name = $_.DnsNameList.Unicode
-    write-host "Certificate with name: $name, friendly_name: $friendly_name, issuer_name: $issuer_name and hash $hash"
-	[System.IO.File]::WriteAllBytes("$output_dir\$friendly_name--$hash.der", $_.export($type) )
+    $der_file = "$output_dir\der\$friendly_name--$hash.der"
+    $pem_file = "$output_dir\pem\$friendly_name--$hash.pem"
+    $crt_file = "$output_dir\crt\$friendly_name--$hash.crt"
+
+    write-host "`nExporting certificate $friendly_name ($hash) to $output_dir"
+
+	[System.IO.File]::WriteAllBytes($der_file, $_.export($type) )
+    certutil -encode -f $der_file $pem_file
+    Copy-Item $pem_file -Destination $crt_file
 }
